84838b0b21ee9abbc2a8c78ca5cb1dddbadf9c34
Edson Souza <edsoncsouza@gmail.com>
2023-12-18 13:31:29 +0000

    ✨ feat: Add module "etcd_certs" for generating etcd certificates
    The commit adds a new module called "etcd_certs" to generate etcd certificates. The module includes configurations for different types of certificates, such as "kube_etcd", "kube_etcd_peer", "kube_etcd_healthcheck_client", and "kube_apiserver_kubelet_client". Each certificate type has specific properties like common name, validity period, certificate uses, hostnames, and IP addresses.
    
    The source for the module is set to "git::github.com/edsoncsouza/vishwakarma.git//modules/tls/certificate". It depends on the "etcd_ca" module provided by the "ca" module. The module uses the private key and certificate from the "etcd_ca" module for certificate signing.
    
    The self_signed flag is set to false, indicating that the certificates will be signed by the CA.
    
    The cert_config block includes common_name, organization, and validity_period_hours properties, which can be customized for each certificate type.
    
    The cert_hostnames and cert_ip_addresses properties are used to specify additional hostnames and IP addresses for each certificate.
    
    The cert_uses property is derived from the provided list of certificate uses and maps them to corresponding local.cert_uses values.
    
    This commit introduces the necessary infrastructure to generate etcd certificates using the "etcd_certs" module.

--- certs.tf
+++ certs.tf
@@ -57,3 +57,47 @@ module "kubernetes_certs" {
   cert_ip_addresses = lookup(each.value, "cert_ip_addresses", [])
   cert_uses         = distinct(concat([for use in lookup(each.value, "cert_uses", []) : local.cert_uses[use]]...))
 }
+
+module "etcd_certs" {
+  for_each = {
+    kube_etcd = {
+      common_name           = "kube-etcd"
+      validity_period_hours = "26280"
+      cert_uses             = ["client", "server"]
+      cert_hostnames        = ["localhost", ]
+      cert_ip_addresses     = ["127.0.0.1", "192.168.6.101", "192.168.6.102"]
+    },
+    kube_etcd_peer = {
+      common_name           = "kube-etcd-peer"
+      validity_period_hours = "26280"
+      cert_uses             = ["client", "server"]
+      cert_hostnames        = ["localhost"]
+      cert_ip_addresses     = ["127.0.0.1", "192.168.6.101", "192.168.6.102"]
+    },
+    kube_etcd_healthcheck_client = {
+      common_name           = "kube-etcd-healthcheck-client"
+      validity_period_hours = "26280"
+      cert_uses             = ["client"]
+    },
+    kube_apiserver_kubelet_client = {
+      common_name = "kube-apiserver-kubelet-client"
+      cert_uses   = ["client"]
+    }
+  }
+
+
+  source = "git::github.com/edsoncsouza/vishwakarma.git//modules/tls/certificate"
+  ca_config = {
+    key_pem  = module.ca["etcd_ca"].private_key_pem
+    cert_pem = module.ca["etcd_ca"].cert_pem
+  }
+  self_signed = false
+  cert_config = {
+    common_name           = lookup(each.value, "common_name", "")
+    organization          = lookup(each.value, "organization", "")
+    validity_period_hours = lookup(each.value, "validity_period_hours", "26280")
+  }
+  cert_hostnames    = lookup(each.value, "cert_hostnames", [])
+  cert_ip_addresses = lookup(each.value, "cert_ip_addresses", [])
+  cert_uses         = distinct(concat([for use in lookup(each.value, "cert_uses", []) : local.cert_uses[use]]...))
+}
